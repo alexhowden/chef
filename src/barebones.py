bb07c7fff1f9129638aa944f5fc4c3df25b90631
Alex Howden <ahowden27@gmail.com>
2025-08-14 05:06:38 -0400

    implemented solution, checking, and everything else

--- src/barebones.py
+++ src/barebones.py
@@ -52,4 +52,34 @@ def test():
 def generate_problem(a: int, b: int):
 	return randrange(pow(10, a - 1), pow(10, a), 1), randrange(pow(10, b - 1), pow(10, b), 1)
 
-print(generate_problem(4, 3))
+# print(generate_problem(4, 3))
+
+
+
+
+
+def solve(a, b):
+	num_cols = len(a) + len(b)
+	solution = []
+	zeros = 0
+	for dig_b in b[::-1]:
+		for x in range(zeros):
+			solution.append(0)
+		tally = 0
+		for dig_a in a[::-1]:
+			print(dig_b, dig_a)
+			prod = int(dig_a) * int(dig_b)
+			added = (prod + tally) % 10
+			solution.append(added)
+			tally = int((prod + tally) / 10)
+			print("prod: ", prod)
+			print("tally: ", tally)
+			print("added: ", added)
+		solution.append(tally)
+		zeros += 1
+		while len(solution) % num_cols != 0:
+			solution.append(0)
+
+	print(solution)
+
+solve("409", "72")
--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ class App(tk.Tk):
 		tk.Tk.__init__(self)
 
 		self.title("chef")
-		self.geometry("700x500")
+		self.geometry("500x500")
 
 		container = ttk.Frame(self)
 		container.pack(side="top", fill="both", expand=True)
@@ -42,6 +42,7 @@ class App(tk.Tk):
 
 		style = ttk.Style(self)
 		style.map("TButton", foreground=[('active', 'blue')])
+		style.configure("TLabel", Activebackground='blue')
 
 	def show_frame(self, cont):
 		frame = self.frames[cont]
@@ -86,6 +87,8 @@ class Problem(tk.Frame):
 
 		num_cols = len(a) + len(b)
 		val_cmd = (self.register(validate_entry), '%P')
+		solution = solve(a, b)
+		focus = 0
 
 		tallies = []
 		for c in range(num_cols - len(a) - 1, num_cols - 1):
@@ -101,11 +104,53 @@ class Problem(tk.Frame):
 			ttk.Label(content, text=val, font=REGFONT, width=1).grid(row=2, column=c+(num_cols-len(seq_b)))
 
 		entries = []
+		checks = []
 		for r in range(3, len(b) + 4):
+			infocus = focus + 3 == r
+
 			for c in range(num_cols):
-				entry = ttk.Entry(content, font=REGFONT, width=1, validate="key", validatecommand=val_cmd)
+				entry = ttk.Entry(content, font=REGFONT, width=1, validate="key", validatecommand=val_cmd, state='potato' if infocus else 'disabled')
 				entry.grid(row=r, column=c)
 				entries.append(entry)
+
+			check = tk.Label(content, text=">" if infocus else '', font=REGFONT, width=1, background='blue' if infocus else content['background'], padx=2)
+			check.grid(row=r, column=num_cols)
+			check.bind("<Button-1>", lambda event, row=r-3: check_row(event, row)) if infocus else check.unbind("<Button-1>")
+			checks.append(check)
+
+		def check_row(event, row):
+			nonlocal focus
+			for i in range((row + 1) * num_cols - 1, row * num_cols - 1, -1):
+				sol_ind = (num_cols * (1 + row * 2) - 1) - i
+				user_val = 0 if entries[i].get() == '' else int(entries[i].get())
+
+				if solution[sol_ind] != int(user_val):
+					print(solution[sol_ind], int(user_val))
+					checks[focus].config(background='red')
+					return
+
+			update_focus()
+
+		def update_focus():
+			nonlocal focus
+
+			checks[focus].config(background='green')
+			checks[focus].unbind("<Button-1>")
+
+			[t.delete(0, tk.END) for t in tallies]
+
+			for c in range(num_cols):
+				entries[focus * num_cols + c].config(state='disabled')
+				if focus != len(b):
+					entries[(focus + 1) * num_cols + c].config(state='potato')
+
+			if focus == len(b):
+				return
+
+			focus += 1
+
+			checks[focus].config(text='>', background='blue')
+			checks[focus].bind("<Button-1>", lambda event, row=focus: check_row(event, row))
 
 def validate_entry(I):
 	return (I.isdigit() and len(I) == 1) or (I == "")
@@ -114,6 +159,34 @@ def generate_problem(a: int, b: int):
 	str_a = str(randrange(pow(10, a -1), pow(10, a), 1))
 	str_b = str(randrange(pow(10, b - 1), pow(10, b), 1))
 	return str_a, str_b
+
+def solve(a, b):
+	num_cols = len(a) + len(b)
+	solution = []
+	zeros = 0
+	for dig_b in b[::-1]:
+		for x in range(zeros):
+			solution.append(0)
+		tally = 0
+		for dig_a in a[::-1]:
+			prod = int(dig_a) * int(dig_b)
+			added = (prod + tally) % 10
+			solution.append(added)
+			tally = int((prod + tally) / 10)
+		solution.append(tally)
+		zeros += 1
+		while len(solution) % num_cols != 0:
+			solution.append(0)
+
+	product = int(a) * int(b)
+	product = str(product)
+	for c in product[::-1]:
+		solution.append(int(c))
+	while len(solution) % num_cols != 0:
+		solution.append(0)
+
+	print(solution)
+	return solution
 
 if __name__ == "__main__":
 	app = App()
