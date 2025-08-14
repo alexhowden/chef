a62eecfe34194948f6b5e2ff3bc5719480292c4d
Alex Howden <ahowden27@gmail.com>
2025-08-14 00:16:07 -0400

    basic ui layout done

--- src/barebones.py
+++ src/barebones.py
@@ -1,47 +1,55 @@
-valid = False
-while not valid:
-	nums = input("Enter your equation (ex: 1024x48): ")
+from random import randrange
 
-	try:
-		a, b = nums.split("x")
-		if int(a) and int(b):
-			valid = True
-	except ValueError:
-		pass
+def test():
+	valid = False
+	while not valid:
+		nums = input("Enter your equation (ex: 1024x48): ")
 
-print(f"""
-
-{a.rjust(len(b) + 2)}
-x {b.rjust(max(0, len(a) - len(b) - 1))}
-{"–" * max(len(a), len(b) + 2)}
-
-""")
-
-int_a = int(a)
-int_b = int(b)
-
-solved = False
-while not solved:
-	next_a = int_a % 10
-	next_b = int_b % 10
-
-	calc = next_a * next_b
-	print(f"calc: {calc}")
-
-	correct = False
-	while not correct:
 		try:
-			val = input(f"{next_a} x {next_b}: ")
-			correct = int(val) == calc
+			a, b = nums.split("x")
+			if int(a) and int(b):
+				valid = True
 		except ValueError:
 			pass
 
-	int_a = int(int_a / 10)
-	if int_a == 0:
-		int_b = int(int_b / 10)
-		int_a = int(a)
+	print(f"""
 
-		if int_b == 0:
-			solved = True
+	{a.rjust(len(b) + 2)}
+	x {b.rjust(max(0, len(a) - len(b) - 1))}
+	{"–" * max(len(a), len(b) + 2)}
 
-print("solved!")
+	""")
+
+	int_a = int(a)
+	int_b = int(b)
+
+	solved = False
+	while not solved:
+		next_a = int_a % 10
+		next_b = int_b % 10
+
+		calc = next_a * next_b
+		print(f"calc: {calc}")
+
+		correct = False
+		while not correct:
+			try:
+				val = input(f"{next_a} x {next_b}: ")
+				correct = int(val) == calc
+			except ValueError:
+				pass
+
+		int_a = int(int_a / 10)
+		if int_a == 0:
+			int_b = int(int_b / 10)
+			int_a = int(a)
+
+			if int_b == 0:
+				solved = True
+
+	print("solved!")
+
+def generate_problem(a: int, b: int):
+	return randrange(pow(10, a - 1), pow(10, a), 1), randrange(pow(10, b - 1), pow(10, b), 1)
+
+print(generate_problem(4, 3))
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
+from random import randrange
 
-FONT = ("Courier New", 35)
+BIGFONT = ("Courier New", 35)
+REGFONT = ("Helvecta", 22)
 
 class App(tk.Tk):
 	def __init__(self):
@@ -24,6 +26,10 @@ class App(tk.Tk):
 
 		self.show_frame(DifficultyPage)
 
+		style = ttk.Style(self)
+
+		style.map("TButton", foreground=[('active', 'blue')])
+
 	def show_frame(self, cont):
 		frame = self.frames[cont]
 		frame.tkraise()
@@ -32,65 +38,161 @@ class DifficultyPage(tk.Frame):
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
 
-		label = ttk.Label(self, text="Select Your Difficulty", font=FONT)
+		label = ttk.Label(self, text="Select Your Difficulty", font=BIGFONT)
 		label.grid(row=0, column=4, padx=10, pady=10)
 
-		diff1 = ttk.Button(self, text="3x2", command = lambda : controller.show_frame(MultDiff1))
-		diff1.grid(row=1, column=1, padx=10, pady=10)
+		diff1 = ttk.Button(self, text="3x2", width=0, command = lambda : controller.show_frame(MultDiff1))
+		diff1.grid(row=1, column=1, padx=5, pady=10)
 
-		diff2 = ttk.Button(self, text="4x2", command = lambda : controller.show_frame(MultDiff2))
-		diff2.grid(row=2, column=1, padx=10, pady=10)
+		diff2 = ttk.Button(self, text="4x2", width=0, command = lambda : controller.show_frame(MultDiff2))
+		diff2.grid(row=2, column=1, padx=5, pady=10)
 
-		diff3 = ttk.Button(self, text="3x3", command = lambda : controller.show_frame(MultDiff3))
-		diff3.grid(row=3, column=1, padx=10, pady=10)
+		diff3 = ttk.Button(self, text="3x3", width=0, command = lambda : controller.show_frame(MultDiff3))
+		diff3.grid(row=3, column=1, padx=5, pady=10)
 
-		diff4 = ttk.Button(self, text="4x3", command = lambda : controller.show_frame(MultDiff4))
-		diff4.grid(row=4, column=1, padx=10, pady=10)
+		diff4 = ttk.Button(self, text="4x3", width=0, command = lambda : controller.show_frame(MultDiff4))
+		diff4.grid(row=4, column=1, padx=5, pady=10)
 
 # 3x2 multiplication
 class MultDiff1(tk.Frame):
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
 
-		label = ttk.Label(self, text="3x2 Multiplication", font=FONT)
-		label.grid(row=0, column=4, padx=10, pady=10)
+		titlebar = tk.Frame(self)
+		titlebar.pack(fill="x", pady=10)
 
-		back = ttk.Button(self, text="<", command = lambda : controller.show_frame(DifficultyPage))
-		back.grid(row=0, column=1, padx=10, pady=10)
+		back = ttk.Button(titlebar, text="<", width=0, command = lambda : controller.show_frame(DifficultyPage))
+		back.pack(side="left", padx=10)
+
+		label = ttk.Label(titlebar, text="3x2 Multiplication", font=BIGFONT)
+		label.pack(side="top")
+
+		a, b = generate_problem(3, 2)
+		print(f"{a} x {b}")
+
+		problem = tk.Frame(self)
+		problem.pack(fill="both", expand=True, pady=20)
+
+		problem.grid_columnconfigure(0, weight=1)
+		problem.grid_columnconfigure(2, weight=1)
+		problem.grid_rowconfigure(0, weight=1)
+		problem.grid_rowconfigure(2, weight=1)
+
+		content = tk.Frame(problem)
+		content.grid(row=1, column=1)
+
+		tally1 = ttk.Entry(content, font=REGFONT, width=1)
+		tally1.grid(row=0, column=1)
+		tally2 = ttk.Entry(content, font=REGFONT, width=1)
+		tally2.grid(row=0, column=2)
+		tally3 = ttk.Entry(content, font=REGFONT, width=1)
+		tally3.grid(row=0, column=3)
+		tally4 = ttk.Entry(content, font=REGFONT, width=1)
+		tally4.grid(row=0, column=4)
+
+		label_s1 = ttk.Label(content, text=' ', font=REGFONT, width=1, borderwidth=2, relief='solid')
+		label_s1.grid(row=1, column=1)
+
+		label_a1 = ttk.Label(content, text=a[0], font=REGFONT, width=1, borderwidth=2, relief='solid')
+		label_a1.grid(row=1, column=2)
+
+		label_a2 = ttk.Label(content, text=a[1], font=REGFONT, width=1, borderwidth=2, relief='solid')
+		label_a2.grid(row=1, column=3)
+
+		label_a3 = ttk.Label(content, text=a[2], font=REGFONT, width=1, borderwidth=2, relief='solid')
+		label_a3.grid(row=1, column=4)
+
+		label_x1 = ttk.Label(content, text='x', font=REGFONT, width=1, borderwidth=2, relief='solid')
+		label_x1.grid(row=2, column=1)
+
+		label_s1 = ttk.Label(content, text=' ', font=REGFONT, width=1, borderwidth=2, relief='solid')
+		label_s1.grid(row=2, column=2)
+
+		label_b1 = ttk.Label(content, text=b[0], font=REGFONT, width=1, borderwidth=2, relief='solid')
+		label_b1.grid(row=2, column=3)
+
+		label_b2 = ttk.Label(content, text=b[1], font=REGFONT, width=1, borderwidth=2, relief='solid')
+		label_b2.grid(row=2, column=4)
+
+		entry10 = ttk.Entry(content, font=REGFONT, width=1)
+		entry10.grid(row=3, column=0)
+		entry11 = ttk.Entry(content, font=REGFONT, width=1)
+		entry11.grid(row=3, column=1)
+		entry12 = ttk.Entry(content, font=REGFONT, width=1)
+		entry12.grid(row=3, column=2)
+		entry13 = ttk.Entry(content, font=REGFONT, width=1)
+		entry13.grid(row=3, column=3)
+		entry14 = ttk.Entry(content, font=REGFONT, width=1)
+		entry14.grid(row=3, column=4)
+
+		entry20 = ttk.Entry(content, font=REGFONT, width=1)
+		entry20.grid(row=4, column=0)
+		entry21 = ttk.Entry(content, font=REGFONT, width=1)
+		entry21.grid(row=4, column=1)
+		entry22 = ttk.Entry(content, font=REGFONT, width=1)
+		entry22.grid(row=4, column=2)
+		entry23 = ttk.Entry(content, font=REGFONT, width=1)
+		entry23.grid(row=4, column=3)
+		entry24 = ttk.Entry(content, font=REGFONT, width=1)
+		entry24.grid(row=4, column=4)
+
+		entry30 = ttk.Entry(content, font=REGFONT, width=1)
+		entry30.grid(row=5, column=0)
+		entry31 = ttk.Entry(content, font=REGFONT, width=1)
+		entry31.grid(row=5, column=1)
+		entry32 = ttk.Entry(content, font=REGFONT, width=1)
+		entry32.grid(row=5, column=2)
+		entry33 = ttk.Entry(content, font=REGFONT, width=1)
+		entry33.grid(row=5, column=3)
+		entry34 = ttk.Entry(content, font=REGFONT, width=1)
+		entry34.grid(row=5, column=4)
 
 # 4x2 multiplication
 class MultDiff2(tk.Frame):
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
 
-		label = ttk.Label(self, text="4x2 Multiplication", font=FONT)
+		label = ttk.Label(self, text="4x2 Multiplication", font=BIGFONT)
 		label.grid(row=0, column=4, padx=10, pady=10)
 
-		back = ttk.Button(self, text="<", command = lambda : controller.show_frame(DifficultyPage))
+		back = ttk.Button(self, text="<", width=0, command = lambda : controller.show_frame(DifficultyPage))
 		back.grid(row=0, column=1, padx=10, pady=10)
+
+		a, b = generate_problem(4, 2)
+		print(f"{a} x {b}")
 
 # 3x3 multiplication
 class MultDiff3(tk.Frame):
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
 
-		label = ttk.Label(self, text="3x3 Multiplication", font=FONT)
+		label = ttk.Label(self, text="3x3 Multiplication", font=BIGFONT)
 		label.grid(row=0, column=4, padx=0, pady=10)
 
 		back = ttk.Button(self, text="<", width=0, command = lambda : controller.show_frame(DifficultyPage))
 		back.grid(row=0, column=1, padx=10, pady=10)
+
+		a, b = generate_problem(3, 3)
+		print(f"{a} x {b}")
 
 # 4x3 multiplication
 class MultDiff4(tk.Frame):
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
 
-		label = ttk.Label(self, text="4x3 Multiplication", font=FONT)
+		label = ttk.Label(self, text="4x3 Multiplication", font=BIGFONT)
 		label.grid(row=0, column=4, padx=10, pady=10)
 
-		back = ttk.Button(self, text="<", command = lambda : controller.show_frame(DifficultyPage))
+		back = ttk.Button(self, text="<", width=0, command = lambda : controller.show_frame(DifficultyPage))
 		back.grid(row=0, column=1, padx=10, pady=10)
 
+		a, b = generate_problem(4, 3)
+		print(f"{a} x {b}")
+
+def generate_problem(a: int, b: int):
+	str_a = str(randrange(pow(10, a -1), pow(10, a), 1))
+	str_b = str(randrange(pow(10, b - 1), pow(10, b), 1))
+	return str_a, str_b
 
 if __name__ == "__main__":
 	app = App()
