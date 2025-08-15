541f6da942b98af2e34a4f326a48d24163294d35
Alex Howden <ahowden27@gmail.com>
2025-08-15 01:52:38 -0400

    save user data to json

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
 from random import randrange
+import json
+import time
 
-FONT_TITLE = ("Courier New", 35)
-FONT_CONTENT = ("Helvetica", 22)
+FONT_TITLE = ('Courier New', 35)
+FONT_CONTENT = ('Courier New', 22)
 
 DIFFICULTIES = [
 	(3,2),
@@ -16,35 +18,37 @@ class App(tk.Tk):
 	def __init__(self):
 		tk.Tk.__init__(self)
 
-		self.title("chef")
-		self.geometry("800x550")
+		self.title('chef')
+		self.geometry('800x550')
 
 		container = ttk.Frame(self)
-		container.pack(side="top", fill="both", expand=True)
+		container.pack(side='top', fill='both', expand=True)
 
 		container.grid_rowconfigure(0, weight=1)
 		container.grid_columnconfigure(0, weight=1)
 
 		self.frames = {}
 
-		for f in (DifficultyPage,):
+		for f in (DifficultyPage, StatsPage):
 			frame = f(container, self)
 			self.frames[f] = frame
-			frame.grid(row=0, column=0, sticky="nsew")
+			frame.grid(row=0, column=0, sticky='nsew')
 
 		for len_a, len_b in DIFFICULTIES:
 			frame = MultiplicationPage(container, self, len_a, len_b)
-			self.frames[f"Mult_{len_a}x{len_b}"] = frame
-			frame.grid(row=0, column=0, sticky="nsew")
+			self.frames[f'Mult_{len_a}x{len_b}'] = frame
+			frame.grid(row=0, column=0, sticky='nsew')
 
 		self.show_frame(DifficultyPage)
 
 		style = ttk.Style(self)
-		style.configure("TButton", font=FONT_CONTENT)
-		style.map("TButton", foreground=[('active', 'blue')])
+		style.configure('TButton', font=FONT_CONTENT)
+		style.map('TButton', foreground=[('active', 'blue')])
 
 	def show_frame(self, cont):
 		frame = self.frames[cont]
+		if cont == StatsPage:
+			frame.refresh()
 		frame.tkraise()
 
 class DifficultyPage(tk.Frame):
@@ -54,34 +58,107 @@ class DifficultyPage(tk.Frame):
 		self.grid_columnconfigure(0, weight=1)
 		self.grid_columnconfigure(2, weight=1)
 		self.grid_rowconfigure(0, weight=1)
-		self.grid_rowconfigure(5, weight=1)
+		self.grid_rowconfigure(6, weight=1)
 
-		label = ttk.Label(self, text="Select Your Difficulty", font=FONT_TITLE)
+		label = ttk.Label(self, text='Select Your Difficulty', font=FONT_TITLE)
 		label.grid(row=0, column=1, padx=10, pady=10)
 
 		for i in range(len(DIFFICULTIES)):
 			a, b = DIFFICULTIES[i]
-			ttk.Button(self, text=f"{a}x{b}", command = lambda f=f"Mult_{a}x{b}": controller.show_frame(f)).grid(row=i+1, column=1, padx=5, pady=10)
+			ttk.Button(self, text=f'{a}x{b}', command = lambda f=f'Mult_{a}x{b}': controller.show_frame(f)).grid(row=i+1, column=1, padx=5, pady=10)
+
+		ttk.Button(self, text='Stats', command = lambda : controller.show_frame(StatsPage)).grid(row=5, column=1, padx=5, pady=10)
+
+class StatsPage(tk.Frame):
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent)
+
+		self.controller = controller
+
+		titlebar = tk.Frame(self)
+		titlebar.pack(fill='x', pady=25)
+
+		data = self.retrieve()
+
+		ttk.Button(titlebar, text='<', width=0, command = lambda : self.controller.show_frame(DifficultyPage)).pack(side='left', padx=10)
+		ttk.Label(titlebar, text=f'Stats for {data['username']}', font=FONT_TITLE).pack(side='top')
+
+		stats = tk.Frame(self)
+		stats.pack(fill='both', expand=True, pady=20)
+
+		stats.grid_columnconfigure(0, weight=1)
+		stats.grid_columnconfigure(2, weight=1)
+		stats.grid_rowconfigure(0, weight=1)
+		stats.grid_rowconfigure(2, weight=1)
+
+		self.content = tk.Frame(stats)
+		self.content.grid(row=1, column=1, padx=10, pady=10)
+
+	def refresh(self):
+		user_data = self.retrieve()
+
+		labels = {}
+
+		label = ttk.Label(self.content, text=f'Balance: {user_data['balance']}', font=FONT_CONTENT)
+		label.grid(row=0, column=0, padx=10, pady=10)
+		labels['balance'] = label
+
+		row = 1
+		for key, value in user_data.items():
+			if key.endswith('answered'):
+				label = ttk.Label(self.content, text=f'{key[0:3]} Questions Answered: {value}', font=FONT_CONTENT)
+				label.grid(row=row, column=0, padx=10, pady=10)
+				labels[key] = label
+				row += 1
+
+	def retrieve(self):
+		try:
+			with open('./src/user_data.json', 'r') as f:
+				user_data = json.load(f)
+		except FileNotFoundError:
+			user_data = { "username": "kid", "3x2_answered": 0, "4x2_answered": 0, "3x3_answered": 0, "4x3_answered": 0, "balance": 0 }
+		return user_data
 
 class MultiplicationPage(tk.Frame):
 	def __init__(self, parent, controller, len_a, len_b):
 		tk.Frame.__init__(self, parent)
 
 		titlebar = tk.Frame(self)
-		titlebar.pack(fill="x", pady=10)
+		titlebar.pack(fill='x', pady=25)
 
-		ttk.Button(titlebar, text="<", width=0, command = lambda : controller.show_frame(DifficultyPage)).pack(side="left", padx=10)
-		ttk.Label(titlebar, text=f"{len_a}x{len_b} Multiplication", font=FONT_TITLE).pack(side="top")
+		ttk.Button(titlebar, text='<', width=0, command = lambda : controller.show_frame(DifficultyPage)).pack(side='left', padx=10)
+		ttk.Label(titlebar, text=f'{len_a}x{len_b} Multiplication', font=FONT_TITLE).pack(side='top')
 
 		a, b = generate_problem(len_a, len_b)
 		self.problem = Problem(self, a, b)
-		self.problem.pack(fill="both", expand=True, pady=20)
+		self.problem.pack(fill='both', expand=True, pady=20)
 
 	def new_problem(self, len_a, len_b):
+		try:
+			with open('./src/user_data.json', 'r') as f:
+				user_data = json.load(f)
+		except FileNotFoundError:
+			user_data = { "username": "kid", "3x2_answered": 0, "4x2_answered": 0, "3x3_answered": 0, "4x3_answered": 0, "balance": 0 }
+
+		dim = f'{len_a}x{len_b}'
+		user_data[f'{dim}_answered'] += 1
+		match dim:
+			case '3x2':
+				user_data['balance'] += .01
+			case '4x2':
+				user_data['balance'] += .02
+			case '3x3':
+				user_data['balance'] += .03
+			case '4x3':
+				user_data['balance'] += .04
+
+		with open('./src/user_data.json', 'w') as f:
+			json.dump(user_data, f)
+
 		self.problem.destroy()
 		a, b = generate_problem(len_a, len_b)
 		self.problem = Problem(self, a, b)
-		self.problem.pack(fill="both", expand=True, pady=20)
+		self.problem.pack(fill='both', expand=True, pady=20)
 		self.problem.update_idletasks()
 
 class Problem(tk.Frame):
@@ -103,7 +180,7 @@ class Problem(tk.Frame):
 
 		tallies = []
 		for c in range(num_cols - len(a) - 1, num_cols - 1):
-			tally = ttk.Entry(content, font=FONT_CONTENT, width=1, validate="key", validatecommand=val_cmd)
+			tally = ttk.Entry(content, font=FONT_CONTENT, width=1, validate='key', validatecommand=val_cmd)
 			tally.grid(row=0, column=c)
 			tallies.append(tally)
 
@@ -120,13 +197,13 @@ class Problem(tk.Frame):
 			infocus = focus + 3 == r
 
 			for c in range(num_cols):
-				entry = ttk.Entry(content, font=FONT_CONTENT, width=1, validate="key", validatecommand=val_cmd, state='normal' if infocus else 'disabled')
+				entry = ttk.Entry(content, font=FONT_CONTENT, width=1, validate='key', validatecommand=val_cmd, state='normal' if infocus else 'disabled')
 				entry.grid(row=r, column=c)
 				entries.append(entry)
 
-			check = tk.Label(content, text=">" if infocus else '', font=FONT_CONTENT, width=1, background='blue' if infocus else content['background'], padx=2)
+			check = tk.Label(content, text='>' if infocus else '', font=FONT_CONTENT, width=1, background='blue' if infocus else content['background'], padx=2)
 			check.grid(row=r, column=num_cols)
-			check.bind("<Button-1>", lambda event, row=r-3: check_row(event, row)) if infocus else check.unbind("<Button-1>")
+			check.bind('<Button-1>', lambda event, row=r-3: check_row(event, row)) if infocus else check.unbind('<Button-1>')
 			checks.append(check)
 
 		def check_row(event, row):
@@ -139,13 +216,8 @@ class Problem(tk.Frame):
 					checks[focus].config(background='red')
 					return
 
-			update_focus()
-
-		def update_focus():
-			nonlocal focus
-
 			checks[focus].config(background='green')
-			checks[focus].unbind("<Button-1>")
+			checks[focus].unbind('<Button-1>')
 
 			[t.delete(0, tk.END) for t in tallies]
 
@@ -161,10 +233,10 @@ class Problem(tk.Frame):
 			focus += 1
 
 			checks[focus].config(text='>', background='blue')
-			checks[focus].bind("<Button-1>", lambda event, row=focus: check_row(event, row))
+			checks[focus].bind('<Button-1>', lambda event, row=focus: check_row(event, row))
 
 def validate_entry(I):
-	return (I.isdigit() and len(I) == 1) or (I == "")
+	return (I.isdigit() and len(I) == 1) or (I == '')
 
 def generate_problem(a: int, b: int):
 	str_a = str(randrange(pow(10, a -1), pow(10, a), 1))
@@ -198,7 +270,7 @@ def solve(a, b):
 
 	return solution
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	app = App()
 	app.mainloop()
 
